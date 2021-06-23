5ef4b4a05f2073fdad085d45fa43e3bf7944f4a6
unknown <jjujju31@naver.com>
2021-06-23 14:49:30 +0900

    2021-06-23

--- 다이나믹프로그래밍1-지인/11052.py
+++ 다이나믹프로그래밍1-지인/11052.py
@@ -2,4 +2,9 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-cards = list(map(int, input().split()))
+P = list(map(int, input().split()))
+dp = [0] * 1001
+for i in range(1, N+1):
+    for j in range(1, i+1):
+        dp[i] = max(dp[i], dp[i-j] + P[j-1])
+print(dp[N])
