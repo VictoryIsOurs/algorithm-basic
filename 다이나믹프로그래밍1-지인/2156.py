1c2e15513742cf0457accb053fffef491539cd68
unknown <jjujju31@naver.com>
2021-07-03 16:13:50 +0900

    2021-07-03

--- 다이나믹프로그래밍1-지인/2156.py
+++ 다이나믹프로그래밍1-지인/2156.py
@@ -1,7 +1,14 @@
 import sys
 input = sys.stdin.readline
 
-'''
-6 10 13 9 8 1
-
-'''
+n = int(input())
+dp = [0]*10001
+wine = [0]
+for i in range(n):
+    wine.append(int(input()))
+dp[1] = wine[1]
+if n > 1:
+    dp[2] = dp[1] + wine[2]
+for i in range(3, n+1):
+    dp[i] = max(dp[i-1], dp[i-3] + wine[i-1] + wine[i], dp[i-2] + wine[i])
+print(dp[n])
