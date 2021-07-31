5cd81633772a5558357a12bc35103fcab666694c
SeonYoung Kim <48751536+SeonYoungKim98@users.noreply.github.com>
2021-07-31 15:13:32 +0900

    210731다시풀기

--- 알고리즘기초-브루트포스/1476.py
+++ 알고리즘기초-브루트포스/1476.py
@@ -1,14 +1,26 @@
+# E S M
+
+#첫째 줄에 E S M으로 표시되는 가장 빠른 연도를 출력한다.
+# 1 1 1은 항상 1이기 때문에, 정답이 음수가 나오는 경우는 없다.
+# 15 x 28 x 19⇒ 7980 개의 조합 얼마 안되니까 그냥 다 해보면 된다 (브루트포스)
+
 E, S, M = map(int, input().split())
 
 e, s, m = 1, 1, 1
-for i in range(15*28*19) :
-    if e == E and s == S and m == M :
-        i += 1
+year = 0
+
+for i in range(15*28*19): #bruteForce부분
+    
+    year +=1
+    
+    if e == E and s == S and m == M: #입력받은 날들이 싹 다 같으면 
+        print(year)
         break
-    if e < 15 : e += 1
-    else : e = 1
-    if s < 28 : s += 1
-    else : s = 1
-    if m < 19 : m += 1
-    else : m = 1
-print(i)
+
+    e+=1
+    s+=1
+    m+=1
+
+    if e == 16: e = 1
+    if s == 29: s = 1
+    if m == 20: m = 1
